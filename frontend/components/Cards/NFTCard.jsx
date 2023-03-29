2898948528dbf4b71bd449ab44db273238261dcb
Navansh <khandelwalnavansh1@gmail.com>
2023-03-29 22:55:21 +0530

    Added ternary conditions

--- frontend/components/Cards/NFTCard.jsx
+++ frontend/components/Cards/NFTCard.jsx
@@ -103,7 +103,7 @@ const NFTCard = ({ url, stake, tokenId, smartAccount }) => {
         <div>
           <section className="text-center max-w-fit border px-3  rounded-md border-[#ffffff82] shadow-lg mx-2 hover:scale-105">
             <h2 className="text-2xl my-2">{nft.name}</h2>
-            <Image src={nft.image} alt={nft.name} width={200} height={400} />
+            <Image src={nft.image ? nft.image : "https://media.raritysniper.com/mutant-ape-yacht-club/4318-600.png?cacheId=2"} alt={nft.name} width={200} height={400} />
             <h2 className="text-md text-[#ffffffbe] mt-2">{nft.desc}</h2>
             <button
               className="bg-[#524ffffb] px-3 py-1 my-3 rounded-md font-medium mb-3 w-[60%] text-lg hover:scale-105"
@@ -117,7 +117,7 @@ const NFTCard = ({ url, stake, tokenId, smartAccount }) => {
         <div>
           <section className="text-center max-w-fit border px-3  rounded-md border-[#ffffff82] shadow-lg hover:scale-105">
             <h2 className="text-2xl my-2">{nft.name}</h2>
-            <Image src={nft.image} alt={nft.name} width={200} height={400} />
+            <Image src={nft.image ? nft.image : "https://media.raritysniper.com/mutant-ape-yacht-club/4318-600.png?cacheId=2"} alt={nft.name} width={200} height={400} />
             <h2 className="text-md text-[#ffffffbe] mt-2">{nft.desc}</h2>
             <button
               className="bg-[#ff0909] px-3 py-1 my-3 rounded-md font-medium mb-3 w-[60%] text-lg hover:scale-105"
